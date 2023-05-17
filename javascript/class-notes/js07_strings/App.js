6dadf0f07754bdf9b6583610a3af3d43fa079ebd
burhan <alacaburhan122016@gmail.com>
2023-05-17 22:27:31 +0300

    search

--- javascript/class-notes/js07_strings/App.js
+++ javascript/class-notes/js07_strings/App.js
@@ -3,7 +3,16 @@
 
 // console.log (result);
 
-let str = "Clarusway"
-console.log(str.charAt(1)); 
-console.log(str.charAt(6)); 
-console.log(str.charAt(str.length-1)); 
+// let str = "Clarusway"
+// console.log(str.charAt(1)); 
+// console.log(str.charAt(6)); 
+// console.log(str.charAt(str.length-1)); 
+
+
+//*örnek;
+let str = 'You do not Know what you do not know until you know.';
+
+console.log (str.search("know"));  //32
+console.log (str.search(/know/i)); //11 To replace case insensitive, use a regular expression with an /i flag (insensitive).
+console.log(str.search('W')); //-1 Capital 'W' is not found in the string
+console.log(str.search(/W/i)); //
