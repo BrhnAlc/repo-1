549dad419dcdb2bd78bc5402c72a01249ac1e545
burhan <alacaburhan122016@gmail.com>
2023-05-17 20:39:39 +0300

    length

---
+++ javascript/class-notes/js07_strings/App.js
@@ -0,0 +1,9 @@
+// let str = 'You do not know what you do not know until you know.';
+// let result = str.indexOf("know", 12); //start search from 12th position
+
+// console.log (result);
+
+let str = "Clarusway"
+console.log(str.charAt(1)); 
+console.log(str.charAt(6)); 
+console.log(str.charAt(str.length-1)); 
